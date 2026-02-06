2b8e4f964665777afa6f40ddd1e0a677d7078f6b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-06 16:58:07 +0100

    chore(app.config.server.ts): format provider array for better readability

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,14 @@
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+// Config.
 import { appConfig } from './app.config';
+// Routes.
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes))]
+  providers: [
+    provideServerRendering(withRoutes(serverRoutes))
+  ]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
