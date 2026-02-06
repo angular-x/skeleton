de2bf7a164c56481b76ad8e698fca37374d9fcea
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-06 17:06:46 +0100

    chore(app.routes.ts): add routing configuration for application components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('@angular.x/dashboard').then(m => m.Dashboard) },
+
+  { path: 'calculations', loadComponent: () => import('@angular.x/calculations').then(m => m.Calculations) },
+
+  { path: 'transactions', loadComponent: () => import('@angular.x/transactions').then(m => m.Transactions) },
+  { path: 'costs', loadComponent: () => import('@angular.x/costs').then(m => m.Costs) },
+  { path: 'expenses', loadComponent: () => import('@angular.x/expenses').then(m => m.Expenses) },
+
+  { path: 'incomes', loadComponent: () => import('@angular.x/incomes').then(m => m.Incomes) },
+
+];
